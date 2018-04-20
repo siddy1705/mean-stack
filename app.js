1356c1e218008b2a64ea8fa04587973d0ebbd685
siddy1705 <vitthaldas.siddharth@gmail.com>
2018-04-20 19:53:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require("mongoose");
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+var url = 'mongodb://localhost:27017/mean-stack';
+
+mongoose.connect("mongodb://localhost:27017/mean-stack") 
+
+app.use(morgan('dev'));
+
+// Routes which should handle requests
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
